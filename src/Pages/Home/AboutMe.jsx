88c4de313d47ca44a0f4a60c2802c2edb442b04e
Makai <danielmakai92@gmail.com>
2025-01-28 13:17:11 +0300

    aboutMe edit

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -13,7 +13,7 @@ export default function AboutMe() {
           <p className="hero--section-description">
            I’m Daniel Makai, a dedicated and enthusiastic Software Engineer with a strong background in full-stack development. I specialize in creating dynamic and efficient web applications using modern technologies like Django, React, Python, and JavaScript. My passion lies in building scalable solutions that solve real-world problems while delivering exceptional user experiences.
 
-I am currently a student at Taita Taveta University taking Bachelor of Science in Information Technology,I have developed a solid foundation in software development and network security. Additionally, my professional experience includes working on various projects, such as e-commerce platforms, student management systems, and AI-driven websites, where I honed my skills in both frontend and backend development.
+I am currently a student at Taita Taveta University taking Bachelor of Science in Information Technology,I have developed a solid foundation in software development and network security. 
 
 Beyond coding, I have a keen interest in learning new technologies and staying updated with industry trends. My journey includes completing programs like the ALX Software Engineering course and the Cisco Networking Academy’s Network Security certification, which have enriched my understanding of software systems and security.
 
@@ -22,7 +22,7 @@ Beyond coding, I have a keen interest in learning new technologies and staying u
           <p className="hero--section-description">
           When I’m not coding, I enjoy collaborating with other developers, mentoring aspiring engineers, and contributing to projects that make a difference. I believe in continuous growth, teamwork, and the power of technology to bring positive change.
 
-Feel free to explore my projects, connect with me, or share ideas—I’m always excited to work on meaningful initiatives and bring ideas to life!
+
 
           </p>
         </div>
