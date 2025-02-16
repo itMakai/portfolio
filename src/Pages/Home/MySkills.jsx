c58d2f773a2ec14716e4576c16097c9ee91ab8db
Makai <danielmakai92@gmail.com>
2025-02-16 19:58:46 +0300

    updated skills component

--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -6,22 +6,20 @@ export default function MySkills() {
   return (
     <section className="skills--section" id="mySkills">
       <div className="portfolio--container">
-      <p className="section--title">My Skills</p>
-     
-      <div className="skills--section--container">
-        
-        {data?.skills?.map((item, index) => (
-          <div key={index} className="skills--section--card">
-            <div className="skills--section--img">
-              <img src={item.src} alt="Product Chain" />
+        <p className="section--title">My Skills</p>
+        <div className="skills--section--container">
+          {data?.skills?.map((item, index) => (
+            <div key={index} className="skills--section--card">
+              <div className="skills--section--img">
+                <img src={item.src} alt={item.title} />
+              </div>
+              <div className="skills--section--card--content">
+                <h3 className="skills--section--title">{item.title}</h3>
+                <p className="skills--section--description">{item.description}</p>
+              </div>
             </div>
-            <div className="skills--section--card--content">
-              <h3 className="skills--section--title">{item.title}</h3>
-              <p className="skills--section--description">{item.description}</p>
-            </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
       </div>
     </section>
   );
